278eea4be72c59c242a2ca8a9b7a86ef1f39cd0b
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2020-03-25 20:27:28 -0300

    Inserting data into database

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.post('/users', (request, response) => {
-  const body = request.body
-  console.log(body)
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body
+  const id = crypto.randomBytes(4).toString('HEX')
 
-  return response.json({
-    evento: 'Semana OmniStack',
-    aluno: 'Cezar Augusto Crummenauer'
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
   })
+
+  return response.json({ id })
 })
 
 module.exports = routes
