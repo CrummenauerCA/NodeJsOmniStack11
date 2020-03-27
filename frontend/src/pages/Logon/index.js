600febd1b1ff91ed29b8107807ac45fc002f28c7
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2020-03-27 16:03:59 -0300

    Creating and styling the register page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -4,20 +4,20 @@ import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css'
 
-import logImg from '../../assets/logo.svg'
+import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 export default function Logon() {
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logImg} alt="Be The Hero" />
+        <img src={logoImg} alt="Be The Hero" />
         <form>
           <h1>Faça seu logon</h1>
           <input type="text" placeholder="Sua ID" />
           <button className="button" type="submit">Entrar</button>
 
-          <Link to="/register">
+          <Link className="back-link" to="/register">
             <FiLogIn size={16} color="e02041" />
             Não tenho cadastro
           </Link>
