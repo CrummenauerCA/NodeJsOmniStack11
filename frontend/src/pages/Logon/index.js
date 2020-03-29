aad46e84897fb6bd573d2bf503452bf5b1694f72
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2020-03-29 17:40:14 -0300

    Redirecting to profile page after login and using localStorage values

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 
 import './styles.css'
@@ -12,6 +12,7 @@ import api from '../../services/api'
 
 export default function Logon() {
   const [id, setId] = useState('')
+  const history = useHistory()
 
   async function handleLogin(event) {
     event.preventDefault()
@@ -20,6 +21,8 @@ export default function Logon() {
       const response = await api.post('sessions', { id })
       localStorage.setItem('ongId', id)
       localStorage.setItem('ongName', response.data.name)
+    
+      history.push('/profile/')
     } catch (error) {
       alert('Falha no login, tente novamente')
     }
