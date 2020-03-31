70c38f42b843c75e5c6af79ef76c21631392df0d
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2020-03-31 02:12:29 -0300

    Sending WhatsApp messages using Linking

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native'
 import * as MailComposer from 'expo-mail-composer'
 
 import styles from './styles'
@@ -11,11 +11,15 @@ import logoImg from '../../assets/logo.png'
 export default function Detail() {
   const navigation = useNavigation()
   const message = 'Olá, APAD. Estou entando em contato, '
-  + 'pois gostaria de ajudar no caso "Cachorro atropelado"'
+  + 'pois gostaria de ajudar no caso "Cachorro atropelado" '
   + 'com o valor de R$ 120,00'
 
   function navigateBack() {
     navigation.goBack()
+  }
+
+  function sendWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=+5551995816271&text=${message}`)
   }
 
   function sendMail() {
@@ -52,7 +56,7 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => { }}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.action} onPress={sendMail}>
