f453180a832537e0cd9a193792e7d2a3f6e4cbc8
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2020-03-31 03:34:03 -0300

    Using infinite scroll

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -13,18 +13,34 @@ import api from '../../services/api'
 
 export default function Incidents() {
   const [incidents, setIncidents] = useState([])
-  const [total, setTotal] = useState([])
+  const [total, setTotal] = useState(0)
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
 
   const navigation = useNavigation()
 
   function navigationToDetail(incident) {
-    navigation.navigate('Detail', {incident})
+    navigation.navigate('Detail', { incident })
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents')
-    setIncidents(response.data)
+    if (loading) {
+      return
+    }
+
+    if (total > 0 && incidents.length === total) {
+      return
+    }
+
+    setLoading(true)
+    const response = await api.get('incidents', {
+      params: { page }
+    })
+    setIncidents([...incidents, ...response.data])
+
     setTotal(response.headers['x-total-count'])
+    setPage(page + 1)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -46,7 +62,9 @@ export default function Incidents() {
       <FlatList style={styles.incidentList}
         data={incidents}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
